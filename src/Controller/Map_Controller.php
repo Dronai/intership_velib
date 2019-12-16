f525054aef3f96149129c917f468be310d8c0c33
Pierre BOUTIN <pierre.boutin@viacesi.fr>
2019-12-16 20:56:26 +0100

    Add request to api

---
+++ src/Controller/Map_Controller.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+class Map_Controller extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function homepage()
+    {
+        return $this->render('homepage.html.twig', [
+            'title' => 'Homepage',
+        ]);
+    }
+
+    /**
+     * @Route("/map", name="map")
+     */
+    public function map()
+    {
+        return $this->render('map.html.twig', [
+            'title' => 'Map Velib',
+            'bornes' => null,
+        ]);
+    }
+
+    /**
+     * @Route("/search_map", name="search")
+     * @throws TransportExceptionInterface
+     * @throws DecodingExceptionInterface
+     */
+    public function search_map()
+    {
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'http://localhost/app/api.php?action=get_list_bornes_zone&address='.$_GET["address"]);
+
+        $statusCode = $response->getStatusCode();
+
+        try {
+            $content = $response->getContent();
+
+            return $this->render('map.html.twig', [
+                'title' => 'Map Velib',
+                'bornes' => $content,
+            ]);
+        } catch (ClientExceptionInterface $e) {
+        } catch (RedirectionExceptionInterface $e) {
+        } catch (ServerExceptionInterface $e) {
+        } catch (TransportExceptionInterface $e) {
+        }
+            return null;
+    }
+}
